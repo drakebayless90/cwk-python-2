0ed62b49dbc5d64381b5729ed8449ea1157cf0aa
student <jojofish3737@gmail.com>
2019-07-22 11:09:27 -0700

    i added 3 + 4

--- first project.py
+++ first project.py
@@ -1,5 +1,33 @@
 def main():
-    print("Hello World")
+    #print("Hello World")
+    #variable = input("what is your name")
+    #print(variable)
+
+    four()
+
+def two():
+    x=10
+    print(x)
+
+def three():
+    num = input("what is your number")
+    print (num)
+    if num > 10:
+        print("greater than 10")
+    if num < 10:
+        print("less than 10")
+    if num == 10:
+        print("equal to 10")
+
+def four():
+    number = input("what's your number")
+    print (number)
+    if number %2 == 1:
+        print("your number is odd")
+    if number %2 == 0:
+        print("your number is even")
+
 
 if __name__ == "__main__":
     main()
+
